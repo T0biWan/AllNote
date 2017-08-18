870bbf68d8b38832dfbe7d5210dc881c9a00f1ba
t0biwan <tobi.klatt@web.de>
2017-08-18 16:35:44 +0200

    Improved conversion, now multiline tas are possible. Every line is part of the tag above until a new tag appears

--- sandbox.py
+++ sandbox.py
@@ -8,12 +8,12 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 allnote_html = {"#": ["<h1>", "</h1>"], "##": ["<h2>", "</h2>"], "###": ["<h3>", "</h3>"], "####": ["<h4>", "</h4>"],
-                "#####": ["<h5>", "</h5>"], "######": ["<h6>", "</h6>"], "+": ["<p>", "</p>"], "*": ["<code>", "</code>"]}
-
+                "#####": ["<h5>", "</h5>"], "######": ["<h6>", "</h6>"], "+": ["<p>", "</p>"],
+                "*": ["<code>", "</code>"]}
 
 
 def get_mark_up(string):
-    logger.debug("return: "+string.split(" ")[0])
+    logger.debug("return: " + string.split(" ")[0])
     return string.split(" ")[0]
 
 
@@ -22,31 +22,52 @@ def trim_mark_up(string):
     for token in string.split(" ")[1:]:
         return_string += token + " "
 
-    logger.debug("return: "+return_string.rstrip())
+    logger.debug("return: " + return_string.rstrip())
     return return_string.rstrip()
 
 
+def line_has_markup(line):
+    return get_mark_up(line) in allnote_html
+
+
 def convert_to_html(string, allnote_tag):
-    return_string = allnote_html[allnote_tag][0] + string + allnote_html[allnote_tag][1]
+    return_string = ""
+    if allnote_tag in allnote_html:
+        return_string = allnote_html[allnote_tag][0] + string + allnote_html[allnote_tag][1]
+
     logger.debug("return: " + return_string)
     return return_string
 
 
-def process_input(string):
-    mark_up = get_mark_up(string)
-    string = trim_mark_up(string)
-    return convert_to_html(string, mark_up)
+def process_input(input):
+    return None
 
 
 def __main__():
     output_file = open("output.txt", "w")
     input_file = open("input.txt", "r")
+
+    current_string = ""
+    mark_up = ""
+
     for line in input_file:
-        output_file.write(process_input(line)+"\n")
+        if line_has_markup(line):
+            if current_string != "":
+                output_file.write(convert_to_html(current_string, mark_up) + "\n")
+            current_string = line
+            mark_up = get_mark_up(line)
+            current_string = trim_mark_up(line)
+        else:
+            current_string += " " + line
+    if current_string != "":
+        output_file.write(convert_to_html(current_string, mark_up))
 
 
 __main__()
 
+
+
 # Er soll immer bis zum nächsten allnote-tag suchen
 # Leere Zeilen sollen aber ignoriert werden
 # Command-line tool draus machen
+# New lines aus String entfernen! Vielleicht gibt es eine Python Anweisung dafür

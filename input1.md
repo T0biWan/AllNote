630e7d3c32e6a5d03a373a1eee4960be66f0b968
t0biwan <tobi.klatt@web.de>
2017-08-27 20:12:06 +0200

    Added Dicitonaries for tags and regexes. Added AllNote Tags and processing to markdown for inline and multiline code

--- input1.md
+++ input1.md
@@ -1,5 +1,2 @@
-* List 0
-* List 0
-
-+ List 1
-+ List 1
+Keine Überschrift
+___
--- sandbox-markdown.py
+++ sandbox-markdown.py
@@ -1,22 +1,36 @@
+import re
 import codecs
 import markdown
 from mdx_gfm import GithubFlavoredMarkdownExtension as gfm
 
+allnote_tags = {"inline-code": "§", "multiline-code": "§§"}
+markdown_tags = {"inline-code": "`", "multiline-code": "```"}
+html_tags = {}
+regex = {"is-inline-code": r"" + allnote_tags["inline-code"] + "(.+)" + allnote_tags["inline-code"],
+         "process-inline-code": r"(" + allnote_tags["inline-code"] + "([^" + allnote_tags["inline-code"] + "]+)" + allnote_tags["inline-code"] + ")+",
+         "is-multiline-code": r"" + allnote_tags["multiline-code"]}
 
+
+# Todo überarbeiten
 def process_allnote(input):
     output = ""
     input_line_by_line = input.split("\n")
     for line in input_line_by_line:
         markup = get_mark_up(line)
         line_without_markup = trim_mark_up(line)
+        if is_inline_code(line):
+            line = process_inline_code(line)
+        if is_multiline_code(line):
+            line = process_multiline_code(line)
         if is_ordered_list(line):
-            output += process_ordered_list(line)
-        else:
-            output += line
+            line = process_ordered_list(line)
+
+        output += line + "\n"
 
     return output
 
 
+# Todo überarbeiten
 def process_sublist(input):
     output = ""
     input_line_by_line = input.split("\n")
@@ -45,6 +59,7 @@ def process_sublist(input):
     return output
 
 
+# Todo überarbeiten
 def process_ordered_list(line):
     markup = get_mark_up(line)
     line_without_markup = trim_mark_up(line)
@@ -52,18 +67,46 @@ def process_ordered_list(line):
     return line + "\n"
 
 
+def process_inline_code(line):
+    pattern = regex["process-inline-code"]
+    replace = r""+markdown_tags["inline-code"]+"\2"+markdown_tags["inline-code"]
+    line = re.sub(pattern, replace, line)
+    return line
+
+
+def process_multiline_code(line):
+    pattern = regex["is-multiline-code"]
+    replace = r""+markdown_tags["multiline-code"]
+    line = re.sub(pattern, replace, line)
+    return line
+
+
+# Todo überarbeiten
 def is_ordered_list(line):
     return line.startswith("+")
 
 
+# Todo überarbeiten
 def is_unordered_list(line):
     return line.startswith("*")
 
 
+def is_multiline_code(line):
+    pattern = regex["is-multiline-code"]
+    return True if re.search(pattern, line) else False
+
+
+def is_inline_code(line):
+    pattern = regex["is-inline-code"]
+    return True if re.search(pattern, line) else False
+
+
+# Todo überarbeiten
 def is_sublist(list_level_current, list_level_line):
     return list_level_current < list_level_line
 
 
+# Todo überarbeiten
 def is_superlist(list_level_current, list_level_line):
     return list_level_current > list_level_line
 
@@ -80,10 +123,7 @@ def trim_mark_up(line):
     return return_string.rstrip()
 
 
-def line_has_markup(line):
-    return get_mark_up(line) in allnote
-
-
+# Todo überarbeiten
 def embed_in_html(input, title, stylesheet):
     html_begin = "<!DOCTYPE html>\n<html lang='en'>\n<head>\n    <meta charset='UTF-8'>\n" \
                  "    <title>" + title + "</title>\n" \
@@ -94,17 +134,18 @@ def embed_in_html(input, title, stylesheet):
     return html_begin + input + html_end
 
 
+# Todo überarbeiten
 def main():
-    input_file = codecs.open("input0.md", mode="r", encoding="utf-8")
+    input_file = codecs.open("input1.md", mode="r", encoding="utf-8")
     output_file = codecs.open("output.html", "w", encoding="utf-8")
 
     input_text = input_file.read()
     title = input_file.name
     css = "css/example.css"
 
-    input_text = process_allnote(input_text)
+    output = process_allnote(input_text)
 
-    output = markdown.markdown(input_text, extensions=[gfm()])
+    output = markdown.markdown(output, extensions=[gfm()])
     output = embed_in_html(output, title, css)
 
     output_file.write(output)
